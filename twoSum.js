aea87583a47053fc7594e4be0b276778317330e1
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-08-06 12:52:49 -0400

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+
+    let numObj = {};
+
+    for (let i = 0; i < nums.length; i++)
+    {
+        let complement = target - nums[i];
+        if (numObj[complement] !== undefined)
+        {
+            return [numObj[complement], i];
+        }
+        numObj[nums[i]] = i;
+    }
+};
